c1071c3b7699e81f00b675105f08da1fc4ae8b5e
최창준 <aowert6518@gmail.com>
2021-07-26 21:47:18 +0900

    Game 클래스 진행 추가 유저 이름을 받고 딜러와 유저 각각 카드 받는 기능까지 추가 (아직 미완)

--- src/src/Game.java
+++ src/src/Game.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
 public class Game {
+    private User user;
+    private User dealer;
+    private Deck deck = new Deck();
     public void start() {
 
     }
 
-    public User inputUser() {
+    public void inputUser() {
         Scanner sc = new Scanner(System.in);
-        User user = new User(sc.next());
-        return user;
+        this.user = new User(sc.next());
+        this.dealer = new User("딜러");
     }
 
+    public void drawCard() {
+        Card card = deck.drawCard();
+        user.addCard(card);
+        Card card1 = deck.drawCard();
+        dealer.addCard(card);
+    }
 
 }
