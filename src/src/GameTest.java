d816e9b438e97235b03385300d1d7c4782e42cd0
최창준 <aowert6518@gmail.com>
2021-07-26 21:47:58 +0900

    여러 확인하고 싶은 테스트 확인 deck생성잘되는지확인, deck이draw하는카드확인, user에카드저장되는지확인,

--- src/src/GameTest.java
+++ src/src/GameTest.java
@@ -6,4 +6,22 @@ public class GameTest {
         Deck deck = new Deck();
         deck.printDeck();
     }
+
+    @Test
+    public void deck이draw하는카드확인() {
+        Deck deck = new Deck();
+        Card card = deck.drawCard();
+        System.out.println(card.getCardPattern() + " " + card.getNumber());
+    }
+
+    @Test
+    public void user에카드저장되는지확인() {
+        User user = new User("chang");
+
+        user.addCard(new Card("다이아몬드", "K"));
+        user.addCard(new Card("클로버", "10"));
+        user.addCard(new Card("스페이드", "A"));
+
+        user.printCards();
+    }
 }
