de5b1a526d807f90b1fc691eec81b1fdf34c1716
최창준 <aowert6518@gmail.com>
2021-07-26 21:07:14 +0900

    Deck 클래스 생성자 및 기능 createOnePatternCardSet(String pattern) 패턴을 입력 받으면 13장의 카드를 만들어 저장하는 함수 createDeck() 4가지 패턴을 입력해서 52장의 카드 덱을 만드는 함수

--- src/src/Deck.java
+++ src/src/Deck.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Deck {
-    HashMap<String, Character> deck = new HashMap<>();
+    private final ArrayList<Card> deck = new ArrayList<>();
 
-    public void
+    public Deck() {
+        createDeck();
+    }
+
+    public void createDeck() {
+        createOnePatternCardSet("다이아몬드");
+        createOnePatternCardSet("하트");
+        createOnePatternCardSet("스페이드");
+        createOnePatternCardSet("클로버");
+    }
+
+    public void createOnePatternCardSet(String pattern) {
+        deck.add(new Card(pattern, "A"));
+        for (int i = 2; i < 11; i++) {
+            deck.add(new Card(pattern, String.valueOf(i)));
+        }
+        deck.add(new Card(pattern, "J"));
+        deck.add(new Card(pattern, "Q"));
+        deck.add(new Card(pattern, "K"));
+    }
+
+    public void printDeck() {
+        for (Card card : deck) {
+            System.out.println(card.cardPattern + " " + card.number);
+        }
+    }
 }
