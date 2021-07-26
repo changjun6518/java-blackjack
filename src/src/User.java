b053ce6653998bb12269fade76d48db913b319d1
최창준 <aowert6518@gmail.com>
2021-07-26 21:45:24 +0900

    User클래스에 손에 쥔 cards 생성 addCard를 사용하여 뽑은 카드를 저장

--- src/src/User.java
+++ src/src/User.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+
 public class User {
     private String userName;
-
+    private ArrayList<Card> cards = new ArrayList<>();
     public User(String userName) {
         this.userName = userName;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void printCards() {
+        for (Card card : cards) {
+            System.out.println(card.getCardPattern() + " " + card.getNumber());
+        }
     }
 }
