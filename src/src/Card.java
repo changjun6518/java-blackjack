2df30a5023785fb2ac4eccf767e58a27addc66b8
최창준 <aowert6518@gmail.com>
2021-07-26 20:51:40 +0900

    Card 클래스 생성자 cardPattern과 number를 String타입으로 지정하여 "스페이드" + "3" 역할을 하도록 함

--- src/src/Card.java
+++ src/src/Card.java
@@ -1,2 +1,9 @@
 public class Card {
+    private String cardPattern;
+    private String number;
+
+    public Card(String cardPattern, String number) {
+        this.cardPattern = cardPattern;
+        this.number = number;
+    }
 }
